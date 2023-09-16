d789430df87effbb6f586278cd0a98fd310043dd
PixelFred0 <99839902+PixelFred0@users.noreply.github.com>
2023-09-16 12:49:13 +0200

    Play 5 Rounds to Finish

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+    var playerScore = 0
+    var computerScore = 0
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const randNumber = Math.floor(Math.random() * 3);
@@ -10,22 +12,28 @@ function singleGame(playerSelection, computerSelection) {
         if (computerSelection == "Rock") {
             return "Its a draw!"
         } else if (computerSelection == "Paper") {
+            computerScore += 1
             return "You Lose! Paper beats Rock"
         } else {
+            playerScore += 1
             return "You Win! Rock beats Scissors"
         };
     } else if (lowerPlayerSelection == "paper") {
         if (computerSelection == "Rock") {
+            playerScore += 1
             return "You Win! Paper beats Rock"
         } else if (computerSelection == "Paper") {
             return "Its a draw!"
         } else {
+            computerScore += 1
             return "You Lose! Scissors beats Paper"
         };
     } else {
         if (computerSelection == "Rock") {
+            computerScore += 1
             return "You Lose! Rock beats Scissors"
         } else if (computerSelection == "Paper") {
+            playerScore += 1
             return "You Win! Scissors beats Paper"
         } else {
             return "Its a draw!"
@@ -33,7 +41,22 @@ function singleGame(playerSelection, computerSelection) {
     };
 }
 
+function game() {
+    let playerSelection = prompt("Your Choice: ")
+    console.log(singleGame(playerSelection, getComputerChoice()))
+    playerSelection = prompt("Your Choice: ")
+    console.log(singleGame(playerSelection, getComputerChoice()))
+    playerSelection = prompt("Your Choice: ")
+    console.log(singleGame(playerSelection, getComputerChoice()))
+    playerSelection = prompt("Your Choice: ")
+    console.log(singleGame(playerSelection, getComputerChoice()))
+    playerSelection = prompt("Your Choice: ")
+    console.log(singleGame(playerSelection, getComputerChoice()))
+    if (playerScore > computerScore) {
+        console.log("You Won! Your Score is ", playerScore)
+    } else {
+        console.log("You Lost! Your Score is ", playerScore)
+    }
+}
 
-const playerSelection = "Scissors"
-const computerSelection = getComputerChoice();
-console.log(singleGame(playerSelection,computerSelection))
+game()
